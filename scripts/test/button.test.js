35071bcb471a6c181c4a20d462e62651f23731f2
TaherCCG <taher.ccg@gmail.com>
2024-03-05 23:58:13 +0000

    Refactor button.test.js to load index.html and add tests for h1 element

--- scripts/test/button.test.js
+++ scripts/test/button.test.js
@@ -5,7 +5,11 @@
 const buttonClick = require("../button");
 
 beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
@@ -13,5 +17,13 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should be present", () => {
+        const h1Element = document.querySelector("h1");
+        expect(h1Element).not.toBeNull();
+    });
+    test("h1 should contain correct text", () => {
+        const h1Element = document.querySelector("h1");
+        expect(h1Element.textContent).toEqual("Jest Click Test");
     });
 });
